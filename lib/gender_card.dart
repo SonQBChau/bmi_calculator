64d45ae265eaa366bd5c7abc465dffe021c12b11
SonChau <soncqb@gmail.com>
2020-01-29 20:18:47 +0700

    add circle gender

--- lib/gender_card.dart
+++ lib/gender_card.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 import 'widget_utils.dart' show screenAwareSize;
 
+double _circleSize(BuildContext context) => screenAwareSize(80.0, context);
+
 class GenderCard extends StatefulWidget {
   final Gender initialGender;
 
@@ -24,9 +26,47 @@ class _GenderCardState extends State<GenderCard> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               CardTitle("GENDER"),
+               Padding(
+                padding: EdgeInsets.only(top: screenAwareSize(16.0, context)),
+                child: _drawMainStack(),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+
+  Widget _drawMainStack() {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        _drawCircleIndicator(),
+      ],
+    );
+  }
+
+    Widget _drawCircleIndicator() {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        GenderCircle(),
+      ],
+    );
+  }
+
+}
+
+class GenderCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: _circleSize(context),
+      height: _circleSize(context),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color.fromRGBO(244, 244, 244, 1.0),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:bmi_calculator/input_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
-      .then((_) => runApp(new MyApp()));
-}
+// void main() {
+//   SystemChrome.setPreferredOrientations(
+//           [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+//       .then((_) => runApp(new MyApp()));
+// }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
